884d32050d1a40727aac10beef1b4e6f4de7bed0
walidata48 <136953190+walidata48@users.noreply.github.com>
2024-11-30 21:33:17 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_secret_key'
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://waliy:12345@localhost/sportv1'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:BvflFpthahDyBvVyeyCzDgEMTqIAVSdN@junction.proxy.rlwy.net:52028/railway'
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
